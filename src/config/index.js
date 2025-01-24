3d5d6fc049421ab1dba0e10eb7e241806ed1d1bc
Alfonso Jesus Pinera Herrera <alfonsojph@correo.ugr.es>
2025-01-24 18:48:11 +0000

    mend

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,11 @@
 let endpointURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001';
 if (!/^http/.test(endpointURL)) {
   endpointURL = 'http://' + endpointURL;
+  console.log('endpointURL:', endpointURL);
 }
-
+if (process.env.REACT_APP_DOMAIN) {
+  endpointURL += '.' + process.env.REACT_APP_DOMAIN;
+}
 const config = {
   endpointURL: endpointURL,
   apiKey: process.env.REACT_APP_API_KEY || 'pcldETTDtJ4CQVz9FqlJYHgIvdYP1iXU'

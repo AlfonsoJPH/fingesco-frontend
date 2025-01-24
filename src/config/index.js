cb16b1c270006ace45f7e17e3a8400256f820e26
Alfonso Jesus Pinera Herrera <alfonsojph@correo.ugr.es>
2025-01-24 18:22:45 +0000

    Fix: add http to hostname if it doesnt specify

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,10 @@
+let endpointURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001';
+if (!/^http/.test(endpointURL)) {
+  endpointURL = 'http://' + endpointURL;
+}
+
 const config = {
-  endpointURL: process.env.REACT_APP_API_BASE_URL || 'http://192.168.1.133:3001',
+  endpointURL: endpointURL,
   apiKey: process.env.REACT_APP_API_KEY || 'pcldETTDtJ4CQVz9FqlJYHgIvdYP1iXU'
 };
 
